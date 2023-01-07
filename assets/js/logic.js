c1cbfd3c9602edc009d63e11b09a5326232cde32
Justin Brueske <justin.brueske@gmail.com>
2023-01-07 16:17:04 -0600

    added hiscore and reload

--- assets/js/logic.js
+++ assets/js/logic.js
@@ -6,13 +6,21 @@ var quizQuestion = document.getElementById("quizquestion");
 var quizEnd =document.getElementById("quizend");
 var quizEndHead =document.getElementById("quizendhead");
 var question = document.getElementById('question');
-var lastAnswer = document.getElementById("lastanswer")
+var lastAnswer = document.getElementById("lastanswer");
+var highscores = document.getElementById("highscores");
+var highScoreList = document.getElementById("highscores-list");
 var a = document.getElementById('a');
 var b = document.getElementById('b');
 var c = document.getElementById('c');
 let i = 0;
 let submitAnswer = '';
+let initialsInput = document.querySelector('#initials');
 buttons = document.getElementsByClassName("answerbutton");
+var submitButton = document.querySelector('#submit');
+var retakeButton = document.querySelector('#retake');
+var clearButton = document.querySelector('#clearscore');
+var hiscore = [];
+
 
 
 function countdown() {
@@ -58,7 +66,7 @@ function startQuestion () {
         a.textContent = quizData[i].a;
         b.textContent = quizData[i].b;
         c.textContent = quizData[i].c;
-        
+
 };
 
 function answer () {
@@ -80,12 +88,9 @@ function answer () {
                     lastAnswer.textContent = "Last Answer was Wrong"
                     nextquestion();
                 } 
-            }
-                 
+            }                 
         );        
-    };
-    
-    
+    };      
 };
 
 function nextquestion () {
@@ -94,9 +99,60 @@ function nextquestion () {
     } else {
         quizQuestion.style.display = "none";
         quizEnd.style.visibility = "visible";
+    }    
+}
+
+function init() {
+    var storedScores = JSON.parse(localStorage.getItem("hiscore"));
+    console.log(storedScores);
+    if (storedScores !== null) {
+        hiscore = storedScores;
     }
     
 }
+
+submitButton.addEventListener('click', function (event) {
+    event.preventDefault();
+    var scores = {
+        person: initialsInput.value,
+        score: timeLeft,
+    }
+    hiscore.push(scores);
+    localStorage.setItem("hiscore", JSON.stringify(hiscore));
+    quizEnd.style.display = "none";
+    highscores.style.visibility = "visible"; 
+    renderHiscore();  
+
+});
+
+function renderHiscore() {
+
+    highScoreList.innerHTML = "";
+    hiscore.sort(function (a, b){return b.score - a.score});
+  
+    for (var i = 0; i < hiscore.length; i++) {
+      var listInitials = hiscore[i].person;
+      var listScore = hiscore[i].score;
+  
+      var li = document.createElement("li");
+      li.textContent = listInitials + ":     " + listScore;
+      li.setAttribute("data-index", i);
+  
+      highScoreList.appendChild(li);
+    }
+}
+
+retakeButton.addEventListener('click', function (event) {
+    location.reload();
+});
+
+clearButton.addEventListener('click', function (event) {
+    localStorage.clear();
+    highScoreList.innerHTML = "";    
+});
+
+init();
+
 
 
 
